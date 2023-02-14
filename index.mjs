81905893858411b2e48ba3dd8d9251e380a164ac
prati <pratima.sampath@gmail.com>
2023-02-14 11:12:29 +0100

    second branch

--- index.mjs
+++ index.mjs
@@ -6,6 +6,8 @@ import { dbConnect } from "./src/db/connectTodb.mjs";
 import register from "./src/db/register.mjs";
 import login from "./src/db/login.mjs";
 import addTudu from "./src/db/addTudu.mjs";
+import jwtAuthentification from "./src/middleware/jwtAuth.mjs";
+import { getTudu } from "./src/db/getTudu.mjs";
 const app = Express();
 dbConnect()
 // connecting to db
@@ -19,7 +21,7 @@ app.use(cookieParser())
 // CORS configuration options
 
 // Use the CORS middleware
-app.use(cors({origin:'http://localhost:3000'}));
+app.use(cors({origin:['http://localhost:3000','https://tudu-bcode.netlify.app/']}));
 
 app.use(
     bodyParser.urlencoded({
@@ -40,7 +42,11 @@ app.post("/register",register)
 app.post("/user/login",login)
 
 //Add a personal Task
-app.post('personal/addTudu',addTudu)
+app.post('/personal/addTudu',addTudu)
+
+//Get a Tudu task
+app.get("/user/getTudu/:id",getTudu)
+
 const port = 4000
 app.listen(port, () => {
     console.log(`Here we go, Engines started at ${port}.`);
--- src/db/addTudu.mjs
+++ src/db/addTudu.mjs
@@ -1,11 +1,29 @@
-// id | user_id | ptaskname | description | category | duedate | entrydate | url | place | style
+// id | user_id | ptaskname | description | category | duedate | entrydate | url | pla
 import { request } from "express";
 import { pool } from "./connectDb.mjs";
 
 const addTudu = async (request,response) =>{
-    const { todoDate, todoCategory, todoItem, email} = request.body;
-    console.log(request.body)
-    response.json( {message:'got data'} )
-    return response.send("got data at backend")
+    try {
+        const {
+            ptaskname,
+            description,
+            category,
+            duedate,
+            entrydate,
+            url,
+            place,
+            user_id
+          } = request.body
+          console.log(request.body)
+          const style = 1
+          const menuQuerry =`INSERT INTO  personaltask (user_id , ptaskname, description , category , duedate , entrydate, url , place,style)
+            VALUES ((SELECT id FROM "user" WHERE login_id =$8 ),$1,$2,$3,$4,$5,$6,$7,${style})`
+            await pool.query(menuQuerry, [ ptaskname,description,category,duedate,entrydate,url,place,user_id])
+            return  response.json({info:"tudu added"})
+        }catch (error) {
+        console.error(error)
+        return response.status(500).send({error:"internal server error"})
+    }
 }
 export default addTudu
+
