19b0d632a31f44c2650db1f6082be3778e53f6e4
andriivam <andriivlasiuk2@gmail.com>
2023-02-22 15:55:39 +0100

    fix errors

--- src/db/connectDb.mjs
+++ src/db/connectDb.mjs
@@ -7,4 +7,28 @@ export const pool = new Pool({
     host : "localhost",
     port : 5432,
     database: "tudu"
-})
+});
+
+
+// const client = (() => {
+//     if (process.env.NODE_ENV === 'production') {
+//         console.log(process.env.NODE_ENV)
+//         return new Client({
+//             connectionString: process.env.DATABASE_URL,
+//             ssl: {
+//                 rejectUnauthorized: false
+//             }
+//         });
+//     } else {
+//         return new Client({
+//             user : process.env.USER,
+//                 password : process.env.PASSWORD,
+//                 host : process.env.HOST,
+//                 port : 5432,
+//                 database: process.env.DATABASE, 
+//             ssl: false
+//         });
+//     }
+// })();
+
+// export default client;
